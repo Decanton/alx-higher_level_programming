b16246f289ec7f1580048f959f535b663f56a453
Dickens Owuor <owuordickens0001@gmail.com>
2023-06-05 12:30:55 -0700

    Hello World

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-will = str[str.index("object"):str.index("programming") + len("programming")]+" "+ "with python"
-print(will)
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
