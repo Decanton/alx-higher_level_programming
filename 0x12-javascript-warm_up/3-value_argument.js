d65aa519217abe0c8a1da0b48da475167af2fa6b
Dickens Owuor <owuordickens00001@gmail.com>
2023-09-11 05:24:21 -0700

    js

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* prints the first argument passed to it */
+
+const args = process.argv.slice(2);
+if (args.length === 0) {
+  console.log('No argument');
+} else {
+  console.log(args[0]);
+}
