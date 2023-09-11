d2aef376d3f23f95ddf37568cec359afd0c8af56
Dickens Owuor <owuordickens00001@gmail.com>
2023-09-11 07:21:14 -0700

    js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,16 +1,6 @@
 #!/usr/bin/node
-/* adds two integers using a function */
-
 function add (a, b) {
   return a + b;
 }
 
-const args = process.argv.slice(2);
-const num1 = parseInt(args[0]);
-const num2 = parseInt(args[1]);
-
-if (!isNaN(num1) && !isNaN(num2)) {
-  console.log(add(num1, num2));
-} else {
-  console.log('Missing or invalid arguments');
-}
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
